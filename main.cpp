9cfde076ef56cf9ead4f023858b2fba307ae92bd
tangezerman <tangezerman@gmail.com>
2024-06-14 22:27:08 +0300

    get time

--- main.cpp
+++ main.cpp
@@ -1,24 +1,38 @@
 #include <iostream>
 #include <ctime>
+#include <string>
+#include <unistd.h> 
 
 
 time_t curr_time;
 
+std::string time_str;
+
 std::string clock_only;
 
-void get_time() {
+
+
+
+
+std::string get_time() {
     curr_time = time(nullptr);
-    std::string time_str = ctime(&curr_time);
-    
-    
-    std::string clock_only = time_str.substr(11, 8);
+    time_str = ctime(&curr_time);
+    clock_only = time_str.substr(11, 8);
+
+
 
     std::cout << clock_only << std::endl;
+
+    return clock_only;
 }
 
 
 int main() {
-    get_time(); 
+    while(true){
+        get_time();
+        sleep(1);
+    }
+    
     return 0;
 }
 
